584d32627208b77c2fa52185a276d929078d7e90
acheird <acheird@gmail.com>
2025-03-31 10:03:47 +0300

    create header component

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="bg-blue-500 text-white p-4">
+      <h1 className="text-lg">InsightEcho Dashboard</h1>
+    </header>
+  );
+};
+
+export default Header;
