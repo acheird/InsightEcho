3398adc54fd6d424dcfa8a33b5114234334bbae6
acheird <acheird@gmail.com>
2025-04-02 12:12:38 +0300

    change bg color

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="bg-blue-500 text-white p-4">
+    <header className="bg-blue-600 text-white p-4 mb-4 w-1/2">
       <h1 className="text-lg">InsightEcho Dashboard</h1>
     </header>
   );
