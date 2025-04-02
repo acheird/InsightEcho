d7f0ad27293e59db0ceb2e3bdd327cd58e9cfc14
acheird <acheird@gmail.com>
2025-04-02 12:11:26 +0300

    add Header component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import ReviewForm from "./components/ReviewForm";
 import Analysis from "./components/Analysis";
+import Header from "./components/Header";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
 
         {/* Main content area on the right */}
         <div className="flex-1 p-6">
+          <Header />
           <Routes>
             <Route path="/" element={<ReviewForm />} />
             <Route path="/reviews" element={<ReviewForm />} />
