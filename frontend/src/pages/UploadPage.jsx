339b8ca4f78484c7c0e12c53335827ce18753a2c
acheird <acheird@gmail.com>
2025-04-11 18:59:48 +0300

    create a new page for uploading reviews

---
+++ frontend/src/pages/UploadPage.jsx
@@ -0,0 +1,11 @@
+import UploadReviews from "../components/UploadReviews";
+
+const UploadPage = () => {
+  return (
+    <div>
+      <UploadReviews />
+    </div>
+  );
+};
+
+export default UploadPage;
