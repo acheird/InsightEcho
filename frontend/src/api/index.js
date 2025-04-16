e2c95231bd8260de5d04765199f977ee343abaee
acheird <acheird@gmail.com>
2025-04-16 14:32:48 +0300

    clean api code from comments

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api"; // Backend URL
+const API_BASE_URL = "http://localhost:5000/api";
 
-// Fetch Sentiment Analysis Data
 export const fetchAnalysis = async () => {
   return axios.get(`${API_BASE_URL}/analysis`);
 };
 
-// Submit a New Review
 export const submitReview = async (review) => {
   return axios.post(`${API_BASE_URL}/reviews`, review);
 };
